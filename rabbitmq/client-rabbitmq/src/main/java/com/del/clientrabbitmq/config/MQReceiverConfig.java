707fa2ad3b48de18000ee45098c95a5baf52e976
may <2320158601@qq.com>
2018-06-09 20:51:16 +0800

    SpringBoot整合RabbitMQ(Topic转发模式

--- rabbitmq/client-rabbitmq/src/main/java/com/del/clientrabbitmq/config/MQReceiverConfig.java
+++ rabbitmq/client-rabbitmq/src/main/java/com/del/clientrabbitmq/config/MQReceiverConfig.java
@@ -1,19 +1,13 @@
 package com.del.clientrabbitmq.config;
 
-import model.User;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MQReceiverConfig {
 
-    @Bean
-    public Queue queue() {
-//        return new Queue("queue_string");
-        return new Queue("queue_obj");
-    }
+/*
+
 
 //    @RabbitListener(queues="queue")    //监听器监听指定的Queue
 //    public void processC(User user) {
@@ -29,6 +23,24 @@ public class MQReceiverConfig {
         System.out.println("Receive  :" + user);
         System.out.println("Receive  :" + user);
     }
+*/
+
+
+//    @Bean
+//    public Queue queue() {
+//        return new Queue("queue");
+////        return new Queue("topic");
+//    }
+
+    @RabbitListener(queues="topic.message")    //监听器监听指定的Queue
+    public void process1(String str) {
+        System.out.println("message:"+str);
+    }
+
+    @RabbitListener(queues="topic.messages")    //监听器监听指定的Queue
+    public void process2(String str) {
+        System.out.println("messages:"+str);
+    }
 }
 
 //2.下面重点介绍一下消费者和生产者之间对象的传输。
@@ -36,3 +48,4 @@ public class MQReceiverConfig {
 //1>.在生产者（项目A）和消费者（项目B）的项目中创建实体类。
 //！注意！：*新建实体类Boy.java 该实体类在项目A、B中的位置，必须一致，即包名必须一致*，在本项目中，Boy.java 在项目A、B中都是： import com.fengdonghao.shiro.bean.Boy;
 //实体类也要一致。
+//https://www.cnblogs.com/hlhdidi/p/6535677.html
