d801aa8b08d8352d58394ba811dbdedfb057fb7c
lamymay <lamymay@outlook.com>
2018-12-26 00:10:19 +0800

    add config

--- springboot-mybatis-plus/src/main/java/com/arc/mybatisplus/service/impl/UserServiceImpl.java
+++ springboot-mybatis-plus/src/main/java/com/arc/mybatisplus/service/impl/UserServiceImpl.java
@@ -1,68 +1,36 @@
-package com.arc.mybatisplus.service.impl;
-
-
-import com.arc.mybatisplus.mapper.FileMapper;
-import com.arc.mybatisplus.model.entries.User;
-import com.arc.mybatisplus.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+package com.baomidou.springboot.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.baomidou.springboot.entity.User;
+import com.baomidou.springboot.mapper.UserMapper;
+import com.baomidou.springboot.service.IUserService;
+
+/**
+ *
+ * User 表数据服务层接口实现类
+ *
+ */
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
-    @Autowired
-    private FileMapper mapper;
+	@Override
+	public boolean deleteAll() {
+		return retBool(baseMapper.deleteAll());
+	}
 
-    @Override
-    public Long save(User user) {
-        mapper.save(user);
-        return user.getId();
-    }
+	@Override
+	public List<User> selectListBySQL() {
+		return baseMapper.selectListBySQL();
+	}
 
-    @Override
-    public User get(Long id) {
-        return mapper.get(id);
-    }
-
-    @Override
-    public User getByFilePath(String filePath) {
-        return mapper.getByFilePath(filePath);
-    }
-
-    @Override
-    public User getByUrl(String url) {
-        return mapper.getByUrl(url);
-    }
-
-    @Override
-    public List<User> list() {
-        List<User> list = mapper.list();
-        System.out.println(list.size());
-        System.out.println(list.size());
-        System.out.println(list.size());
-        System.out.println(list.size());
-        return list;
-    }
-
-    @Override
-    public int delete(Long id) {
-        return mapper.delete(id);
-    }
-
-    @Override
-    public int update(User user) {
-        return mapper.update(user);
-    }
-
-    @Override
-    public User getByFilename(String filename) {
-        return mapper.getByFilename(filename);
-    }
-
-    @Override
-    public User getLatest() {
-        return mapper.getLatest();
-    }
+	@Override
+	public List<User> selectListByWrapper(Wrapper wrapper) {
+		return baseMapper.selectListByWrapper(wrapper);
+	}
 }
