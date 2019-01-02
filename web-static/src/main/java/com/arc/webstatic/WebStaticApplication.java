28b16656903b3c38e0e612f76705bcc8f2adb179
lamymay <lamymay@outlook.com>
2019-01-02 23:56:02 +0800

    add security

---
+++ web-static/src/main/java/com/arc/webstatic/WebStaticApplication.java
@@ -0,0 +1,14 @@
+package com.arc.webstatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStaticApplication.class, args);
+    }
+
+}
+
