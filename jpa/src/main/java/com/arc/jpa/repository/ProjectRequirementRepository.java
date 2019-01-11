f0b501b9d6122f75724b6605a7fb374675a552c8
lamymay <lamymay@outlook.com>
2019-01-11 20:31:02 +0800

    all

---
+++ jpa/src/main/java/com/arc/jpa/repository/ProjectRequirementRepository.java
@@ -0,0 +1,23 @@
+package com.arc.jpa.repository;
+
+import com.arc.jpa.model.ProjectRequirement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.Column;
+
+/**
+ * @description:
+ * @author: yechao
+ * @date: 2019/1/10 15:04
+ */
+public interface ProjectRequirementRepository extends JpaRepository<ProjectRequirement, Long> {
+//
+
+
+    /**
+     * 应用例子
+     */
+    @Query(value = "SELECT * FROM `t_project_requirement` WHERE apply_example=?1  AND  note =?2", nativeQuery = true)
+    ProjectRequirement getByApplyExampleAndNote(String applyExample, String note);
+}
