5f29b5a6f7017bcc15bfe28de8d65730e5d35232
lamymay <lamymay@outlook.com>
2019-01-29 22:44:08 +0800

    init security in memory

---
+++ security/security-1/src/main/java/com/arc/securityinmemory/SecurityInMemoryApplication.java
@@ -0,0 +1,15 @@
+package com.arc.securityinmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityInMemoryApplication {
+
+    //https://blog.csdn.net/liushangzaibeijing/article/details/81220610
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityInMemoryApplication.class, args);
+    }
+
+}
+
--- security/security-1/src/main/java/com/arc/securityinmemory/controller/IndexController.java
+++ security/security-1/src/main/java/com/arc/securityinmemory/controller/IndexController.java
@@ -1,38 +1,19 @@
 package com.arc.securityinmemory.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author: yechao
- * @date: 2019/1/2 23:24
- * @description:
+ * druid 监控配置
  */
-@Controller
+@RestController
 public class IndexController {
 
-
-    @GetMapping(value = "/{id}")
-    public String user(@PathVariable Long id) {
-        return "index";
-    }
-
-    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
-    public String test(@PathVariable Long id) {
-        return "index";
-    }
-
-    @RequestMapping(value = "/index", method = {RequestMethod.GET, RequestMethod.POST})
-    public String index() {
-        return "index";
-    }
-
-    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
-    public String login() {
-        return "login";
+    @RequestMapping("/index")
+    public String index(Model model) {
+        System.out.println("#######");
+        return "index.html";
     }
 
 
