ca6ca5623a697c3f5a4cbb6ddea58e157f06b569
may <lamymay@outlook.com>
2019-03-26 22:09:02 +0800

    add some test 2

---
+++ test/src/main/java/com/arc/test/test/optional/OptionalTest.java
@@ -0,0 +1,16 @@
+package com.arc.poi.controller.test;
+
+import java.util.Optional;
+
+/**
+ * @author: 叶超
+ * @since: 2019/2/18 9:17
+ */
+public class OptionalTest {
+
+    public void setName(String name) throws IllegalArgumentException {
+        name = Optional.ofNullable(name).filter(User::isNameValid).orElseThrow(() -> new IllegalArgumentException("Invalid username."));
+    }
+
+}
+
---
+++ test/src/main/java/com/arc/test/test/optional/User.java
@@ -0,0 +1,13 @@
+package com.arc.poi.controller.test;
+
+/**
+ * @author: 叶超
+ * @since: 2019/2/18 9:18
+ */
+public class User {
+
+    public static boolean isNameValid(String s) {
+        return false;
+    }
+
+}
