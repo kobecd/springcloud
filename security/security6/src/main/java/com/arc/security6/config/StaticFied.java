fcf5fef5eb8914a7ffaaa475172381f0031ffe5b
may <lamymay@outlook.com>
2019-05-11 22:43:24 +0800

    或验证码图片 2

--- security/security6/src/main/java/com/arc/security6/config/StaticFied.java
+++ security/security6/src/main/java/com/arc/security6/config/StaticFied.java
@@ -14,7 +14,5 @@ public class StaticFied {
     public static final String loginUrl = "/testLogin";
     public static final String verifyCode = "/verify/code";
     public static final String KEY_FOR_COOKIE = "H4S5H6A7C1P6K";
-
-
     public static final String KEY_PRIFIX_FOR_VERIFY_CODE ="KEY_FOR_VERIFY_CODE@";
 }
