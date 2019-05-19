8106b4ae0173510eb19532e2ca8935e859b05443
may <lamymay@outlook.com>
2019-05-19 11:16:18 +0800

    体验spring security，完全都是默认配置。

--- security/security1/src/main/java/com/arc/security1/SecurityInMemoryApplication.java
+++ security/security1/src/main/java/com/arc/security1/SecurityInMemoryApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * //https://blog.csdn.net/liushangzaibeijing/article/details/81220610
- * security.basic.enabled 配置过时或不可用
- * 简单粗暴方法：
+ * 安全框架 初学:使用的是全默认配置
+ * 这一项目目的：
+ * 1、体验spring security
+ * 2、security.basic.enabled 配置是否启用，
+ * 2.1、过时或不可用的解决方案（简单粗暴方法）：https://blog.csdn.net/liushangzaibeijing/article/details/81220610
  * 把 Security包从pom.xml中移出去
- * 科学一点的：
+ * 2.2、排除自动配置（科学一点的）：
  * 在Application启动类上（或者任意@Configure配置类上）增加如下注解：
- * 每次启动后在控制台中会生成一个密码，账号为“user”
+ * 注意：每次启动后在控制台中会生成一个密码，账号为“user”
  * Using generated security password: b815bb0b-32fe-4100-b02f-1161a469b023
  */
 @SpringBootApplication(
